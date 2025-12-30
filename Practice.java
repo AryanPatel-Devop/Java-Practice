c6dc617d7ac150cb9c1d8f2c87df4068adfe412d
Aryan <aryanpatel8224@gmail.com>
2025-12-30 19:13:08 +0530

    first commit

---
+++ Practice.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class power{
+    int num;
+    int pow;
+
+    int calcPower(power p){
+        int mult = p.num;
+        for(int i = 0 ; i<(p.pow - 1) ; i++){
+            mult = mult*p.num;
+        }
+        return mult;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        power p = new power();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of num");
+        p.num = sc.nextInt();
+
+        System.out.println("Enter power");
+        p.pow = sc.nextInt();
+
+        int result = p.calcPower(p);
+        System.out.println("Result: " + result);
+    }
+}
